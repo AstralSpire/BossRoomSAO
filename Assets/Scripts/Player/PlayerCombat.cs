a1bd76ed6133dbbadb70b0a284a05cb54e678d32
GenetixxUTD <cameronshirley8@googlemail.com>
2024-10-05 16:02:11 +0100

    Imported Kirito Swords by MWL055
    https://sketchfab.com/3d-models/kirito-swords-d638be6b146e4bc0a14e814c534b418b#download

---
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    [SerializeField] private float swingRate = 2.0f;
+    private bool canSwing = true;
+
+    //[SerializeField] private Animator swordAnimator;
+    [SerializeField] private Camera playerCamera;
+    //[SerializeField] private AudioSource swingSFX;
+    
+}
